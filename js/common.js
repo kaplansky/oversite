ef4dce8df02e761a2034a64cc8b4b84f5c6e92e6
leon2796 <leon2796@gmail.com>
2016-11-14 14:00:59 +0300

    update

--- js/common.js
+++ js/common.js
@@ -61,3 +61,29 @@ $(window).load(function() {
 	$(".loader").delay(400).fadeOut("slow");
 
 });
+
+$(document).ready(function() {
+    var leftUIEl = $('.arr_left');
+    var rightUIEl = $('.arr_right');
+    var elementsList = $('.images');
+ 
+    var pixelsOffset = 125;
+    var currentLeftValue = 0;
+    var elementsCount = elementsList.find('li').length;
+    var minimumOffset = - ((elementsCount - 5) * pixelsOffset);
+    var maximumOffset = 0;
+ 
+    leftUIEl.click(function() {        
+        if (currentLeftValue != maximumOffset) {
+            currentLeftValue += 125;
+            elementsList.animate({ left : currentLeftValue + "px"}, 500);
+        }        
+    });
+ 
+    rightUIEl.click(function() {        
+        if (currentLeftValue != minimumOffset) {
+            currentLeftValue -= 125;
+            elementsList.animate({ left : currentLeftValue + "px"}, 500);
+        }        
+    });
+});
