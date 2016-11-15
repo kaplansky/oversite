bcf791828a49287c21fea8638082d2b03ab8effb
leon2796 <leon2796@gmail.com>
2016-11-15 15:01:01 +0300

    update

--- js/common.js
+++ js/common.js
@@ -61,29 +61,3 @@ $(window).load(function() {
 	$(".loader").delay(400).fadeOut("slow");
 
 });
-
-$(document).ready(function() {
-    var leftUIEl = $('.arr_left');
-    var rightUIEl = $('.arr_right');
-    var elementsList = $('.images');
- 
-    var pixelsOffset = 125;
-    var currentLeftValue = 0;
-    var elementsCount = elementsList.find('li').length;
-    var minimumOffset = - ((elementsCount - 5) * pixelsOffset);
-    var maximumOffset = 0;
- 
-    leftUIEl.click(function() {        
-        if (currentLeftValue != maximumOffset) {
-            currentLeftValue += 125;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }        
-    });
- 
-    rightUIEl.click(function() {        
-        if (currentLeftValue != minimumOffset) {
-            currentLeftValue -= 125;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }        
-    });
-});
